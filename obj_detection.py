63ad19172e2b1a3c6c64133587f49550bad244af
Yueheng0628 <64717236+Yueheng0628@users.noreply.github.com>
2022-07-02 11:09:57 -0400

    Add files via upload

--- obj_detection.py
+++ obj_detection.py
@@ -1,31 +1,35 @@
 import torch
-from matplotlib import pyplot as plt
-import numpy as np
 import cv2
+import pandas as pd
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5x6')
 
-for i in range(0, 1):
-    content = 'Formated_Data\Experiment_2/Contents/' + str(i) + '.mp4'
+for i in range(2, 9):
+    ObjList = []
+    content = 'Formated_Data\\Experiment_1\\Contents\\' + str(i) + '.mp4'
+    outputPath = 'Formated_Data\\Experiment_1\\Contents\\obj_video' + str(i) + '.csv'
     cap = cv2.VideoCapture(content)
     print("============>>>>>>>>path: ", content)
     for frame_idx in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
+        if frame_idx % 500 == 0:
+            print(int(frame_idx / int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) * 100), '%', end=" ")
         ret, frame = cap.read()
-        cv2.namedWindow('video player', cv2.WINDOW_NORMAL)
+        # cv2.namedWindow('video player', cv2.WINDOW_NORMAL)
         results = model(frame)
         # results.print()
         # results.xyxy[0]  # im predictions (tensor)
         data = results.pandas().xyxy[0]  # im predictions (pandas)
         for ind in data.index:
-            if data['confidence'][ind] > 0.5:
-                print(data['xmin'][ind], data['ymin'][ind], data['xmax'][ind], data['ymax'][ind], data['name'][ind], data['confidence'][ind])
-                frame = cv2.rectangle(frame, (int(data['xmin'][ind]), int(data['ymin'][ind])),
-                                      (int(data['xmax'][ind]), int(data['ymax'][ind])),
-                                      (255, 0, 0), 5)
-                frame = cv2.putText(frame, data['name'][ind], (int(data['xmin'][ind])-10, int(data['ymin'][ind])-10),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 2, cv2.LINE_AA)
-        cv2.imshow('video player', frame)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+            temp = [frame_idx, data['xmin'][ind], data['ymin'][ind], data['xmax'][ind], data['ymax'][ind],
+                    data['name'][ind], data['confidence'][ind]]
+            ObjList.append(temp)
+        # cv2.imshow('video player', frame)
+        # if cv2.waitKey(10) & 0xFF == ord('q'):
+        #     break
+    # cap.release()
+    # cv2.destroyAllWindows()
+    ObjPan = pd.DataFrame(ObjList,
+                          columns=['Frame_idx', 'Obj_xmin', 'Obj_ymin', 'Obj_xmax', 'Obj_ymax',
+                                   'Obj_name', 'Obj_confidence'])
+    ObjPan.to_csv(outputPath, index=False)
+    print("\n============>>>>>>>>output path: ", outputPath)
